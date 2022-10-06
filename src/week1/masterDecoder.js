d0ce4a0b944842e53c237bfb3adb10ba48eecdf8
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-10-06 12:41:15 -0700

    Add masterDecoder implementation

---
+++ src/week1/masterDecoder.js
@@ -0,0 +1,27 @@
+import fix from './day1.js';
+import aBitOfAMess from './day2.js';
+import { getDataFromFile, giveItSomeSpace } from './day3.js';
+import reverseIt from './day4.js';
+import catThemedPunFilter from './day5.js';
+import removeDecoy from './day6.js';
+
+const encryptedMsg = "e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs";
+// day1
+const fixedEncryptedMsg = fix(encryptedMsg);
+// day2
+const theMessData = getDataFromFile('theMess.txt');
+const keyCharacters = aBitOfAMess(theMessData);
+// day3
+const spacedEncryptedMsg = giveItSomeSpace(fixedEncryptedMsg, keyCharacters);
+// day4
+const reversedSpacedEncryptedMsg = reverseIt(spacedEncryptedMsg);
+// day5
+const countOfValidPuns = getDataFromFile('bestTitlesEver.txt')
+                        .split(',')
+                        .filter(title => catThemedPunFilter(title))
+                        .length;
+
+// day6
+const noDecoyEncryptedMsg = removeDecoy(reversedSpacedEncryptedMsg, countOfValidPuns);
+
+console.log(noDecoyEncryptedMsg); // -> sfmglyvi 1 DVVP WLDM. #100wVEH dv tl tvg!!
