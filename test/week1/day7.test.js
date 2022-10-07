9b6c632e72dc3fe4971ae24fec4aa1726efc7d2a
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-10-07 11:07:05 -0700

    Add tests week1/day7

---
+++ test/week1/day7.test.js
@@ -0,0 +1,20 @@
+import swapToInverse from "../../src/week1/day7"
+import { describe, expect, it } from "vitest";
+
+describe("#swapToInverse", () => {
+
+    let s = "vCZNKOV";
+    // ex: given 'A', its inverse is -> 'z' 
+    it("Swaps each letter of the alphabet to its inverse", () => {
+        expect(swapToInverse(s)).toBe("Example");
+    });
+});
+
+describe("#letterSwap", () => {
+
+    let s = "0 MFNYVI LI KFMXGFZGRLM XSZMTVH";
+
+    it("Swaps each letter of the alphabet to its inverse", () => {
+        expect(swapToInverse(s)).toBe("Example: 0 number or punctuation changes");
+    });
+});
