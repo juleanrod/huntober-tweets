6cff71681da9e8a971f6753c3e31ad2984cbec03
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-10-07 11:55:59 -0700

    Add day7 to masterDecoder

--- src/week1/masterDecoder.js
+++ src/week1/masterDecoder.js
@@ -4,6 +4,7 @@ import { getDataFromFile, giveItSomeSpace } from './day3.js';
 import reverseIt from './day4.js';
 import catThemedPunFilter from './day5.js';
 import removeDecoy from './day6.js';
+import swapToInverse from './day7.js';
 
 const encryptedMsg = "e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs";
 // day1
@@ -23,5 +24,6 @@ const countOfValidPuns = getDataFromFile('bestTitlesEver.txt')
 
 // day6
 const noDecoyEncryptedMsg = removeDecoy(reversedSpacedEncryptedMsg, countOfValidPuns);
-
-console.log(noDecoyEncryptedMsg); // -> sfmglyvi 1 DVVP WLDM. #100wVEH dv tl tvg!!
+// day7
+const decryptedMsg = swapToInverse(noDecoyEncryptedMsg);
+console.log(decryptedMsg); // -> HUNTOBER 1 week down. #100Devs WE GO GET!!
