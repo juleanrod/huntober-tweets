b333105cf13a2b08ba6b9252293d06074340274d
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-10-10 21:52:02 -0700

    Add edge case to week2/day3

--- src/week2/day3.js
+++ src/week2/day3.js
@@ -1,7 +1,7 @@
 export default function moveThingsAround(array) {
     
     let bucket1 = array.filter((x) => { return x.includes('a') });
-    let bucket2 = array.filter((x) => { return x.length <= 3 });
+    let bucket2 = array.filter((x) => { return x.length <= 3 && !x.includes('a')});
     let bucket3 = array.filter((x) => { return x.length > 3 && !x.includes('a')});
 
     return bucket1.concat(bucket2).concat(bucket3);
