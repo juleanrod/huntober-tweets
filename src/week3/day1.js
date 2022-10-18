138334430b553975af4376b226e564a9961d5a98
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-10-17 19:06:00 -0700

    Add week3/day implementation

---
+++ src/week3/day1.js
@@ -0,0 +1,8 @@
+function myExampleFunction(arr) {
+    if (arr === null || arr.length === 0) return arr;
+    if (arr[0].length !== 0 && arr.length >= 10) arr.push('maximum');
+    if (arr[0] === 'must be maximum') arr.push('failure');
+    if (arr[0] === 'flexible') arr.push('success');
+
+    return arr;
+}
