e2137ade956e3c2a92c8bfdb285f03d672072e8a
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-10-18 11:40:14 -0700

    Refactor week3/day1 implementation

--- src/week3/day1.js
+++ src/week3/day1.js
@@ -1,8 +1,11 @@
 function myExampleFunction(arr) {
+
     if (arr === null || arr.length === 0) return arr;
     if (arr[0].length !== 0 && arr.length >= 10) arr.push('maximum');
-    if (arr[0] === 'must be maximum') arr.push('failure');
-    if (arr[0] === 'flexible') arr.push('success');
+    else {
+        if (arr[0] === 'must be maximum') arr.push('failure');
+        if (arr[0] === 'flexible') arr.push('success');
+    }
 
     return arr;
 }
